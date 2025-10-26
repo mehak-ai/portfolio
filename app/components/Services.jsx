ef292242508ea37fe1f13fb8230f28ad5debc9b4
Mehak Sharma <mehaksharmastu@gmail.com>
2025-10-26 23:46:16 +0530

    Replace <img> tags with <Image> component for optimized image handling

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,5 +1,6 @@
 import { assets, serviceData } from '@/assets/assets'
 import React from 'react'
+import Image from 'next/image'
 
 const Services = () => {
   return (
@@ -19,11 +20,11 @@ const Services = () => {
                        hover:bg-lighthover hover:-translate-y-1 hover:shadow-[2px_2px_6px_rgba(0,0,0,0.15)]
                        duration-500 transition-all'
           >
-            <img src={icon} alt={title} className='w-10' />
+            <Image src={icon} alt={title} className='w-10 h-10 object-contain' width={40} height={40} />
             <h3 className='text-lg my-4 text-gray-700 font-Ovo'>{title}</h3>
             <p className='text-sm text-gray-600 leading-5'>{description}</p>
             <a href={link} className='flex items-center gap-2 text-sm mt-5 text-gray-700 hover:text-darkhover'>
-              Read more <img src={assets.right_arrow} alt="arrow" className='w-4' />
+              Read more <Image src={assets.right_arrow} alt="arrow" className='w-4 h-4' width={16} height={16} />
             </a>
           </div>
         ))}
