3a222bc8b8e095471b3eb29901c872e0d8b219f7
Mehak Sharma <mehaksharmastu@gmail.com>
2025-11-28 00:44:18 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: __dirname,   // <--- This FIXES WRONG ROOT DETECTION
+  },
 };
 
 export default nextConfig;
